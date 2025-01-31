2890d38cb1a063f00fef4de6255d8912db9f073c
kartik132000 <sujeetkumarsen37@gmail.com>
2025-01-31 20:06:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+  document.addEventListener("DOMContentLoaded", function () {
+            const sizeInfo = document.getElementById("sizeInfo");
+            const h1 = document.createElement("h1");
+            sizeInfo.appendChild(h1);
+
+            function updateSize() {
+                h1.textContent = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+            }
+
+            updateSize(); // Initial size update
+            window.addEventListener("resize", updateSize);
+        });
